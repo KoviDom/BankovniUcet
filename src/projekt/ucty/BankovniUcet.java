2a96e9843aad567676828cc0e04cc682b3bb120f
KoviDom <dominik.kovac01@seznam.cz>
2025-02-26 17:17:42 +0100

    metody

--- src/projekt/ucty/BankovniUcet.java
+++ src/projekt/ucty/BankovniUcet.java
@@ -15,4 +15,24 @@ public abstract class BankovniUcet {
         this.vlastnik = vlastnik;
     }
 
+    //dodelat podminky jak vklad i vyber
+    public void vklad(double castka) {
+        System.out.println("Vložil jste: " + castka + zustatek);
+    }
+
+    public void vyber(double castka) {
+        System.out.println("Vybrali jste: " + castka);
+        System.out.println("Zůstatek: " + zustatek);
+    }
+
+    public void prevodNa(BankovniUcet cilovyUcet, double castka) {
+        if (this.zustatek >= castka && castka > 0) {
+            this.zustatek -= castka; // Odečteme částku z tohoto účtu
+            cilovyUcet.zustatek += castka; // Přičteme částku na cílový účet
+            System.out.println("Převod " + castka + " CZK na účet " + cilovyUcet.cisloUctu + " úspěšný.");
+        } else {
+            System.out.println("Nedostatek peněz na účtě nebo neplatná částka.");
+        }
+    }
+
 }
